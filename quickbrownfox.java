612d1afd0b36311a5d7ffffdfaa86c7afed49581
lindsay-greene <lindsay.smile@yahoo.com>
2020-11-25 14:22:07 -0500

    commit changes

---
+++ quickbrownfox.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class quickbrownfox {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
