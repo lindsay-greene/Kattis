63b5006f481020b903962315ff20337ec6c0efd4
lindsay-greene <lindsay.smile@yahoo.com>
2020-11-25 15:12:11 -0500

    accepted

--- quickbrownfox.java
+++ quickbrownfox.java
@@ -4,7 +4,23 @@ import java.util.*;
 public class quickbrownfox {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-
+        int n = Integer.parseInt(input.nextLine());
+        for(int i = 0; i < n; i++) {
+            int[] count = new int[26];
+            String phrase = input.nextLine();
+            phrase = phrase.toLowerCase();
+            for(int j = 0; j < phrase.length(); j++) {
+                int ascii = (int)phrase.charAt(j);
+                if(ascii >= 97 && ascii <= 122) {
+                    count[ascii - 'a'] = 1;
+                }
+            }
+            String missing = "";
+            for(int j = 0; j < count.length; j++) {
+                if(count[j] == 0) missing += (char)(j + 'a');
+            }
+            if(missing.length() == 0) System.out.println("pangram");
+            else System.out.println("missing " + missing);
+        }
     }
-
 }
