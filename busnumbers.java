b65b73013dc7c2584074f8b51adbd6db21ef0715
lindsay-greene <lindsay.smile@yahoo.com>
2020-12-10 22:23:46 -0500

    accepted

--- busnumbers.java
+++ busnumbers.java
@@ -8,12 +8,18 @@ public class busnumbers {
         int[] buses = new int[n];
         for(int i = 0; i < n; i++) buses[i] = input.nextInt();
         Arrays.sort(buses);
-        for(int i = 0; i < buses.length-1; i++) {
-            int first = buses[i];
-            int last;
-            while(buses[i] + 1 == buses[i+1]) {
-
-            }
+        String ans = "" + buses[0];
+        for(int i = 1; i < n; i++) {
+            if(buses[i] == buses[i-1]+1) {
+                if(n > i+1 && buses[i+1] == buses[i]+1) {
+                    ans += "-";
+                    i += 2;
+                    while(n > i && buses[i] == buses[i-1]+1) i++;
+                    i--;
+                    ans += buses[i];
+                } else ans += " " + buses[i];
+            } else ans += " " + buses[i];
         }
+        System.out.println(ans);
     }
 }
